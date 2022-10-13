c8513759939222ed6a9d3209e66a85c2f99fdc0a
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2022-10-13 22:51:16 +0800

    Refactor add question mark operator

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -23,12 +23,8 @@ use std::num::ParseIntError;
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
-    let cost = match qty {
-        Ok(number)=>qty.unwrap() * cost_per_item + processing_fee,
-        Err(e)=>return Err(e)
-    };
-    Ok(cost)
+    let qty = item_quantity.parse::<i32>()?;
+    Ok(qty * cost_per_item + processing_fee)
     
 }
 
@@ -49,3 +45,4 @@ mod tests {
         );
     }
 }
+
